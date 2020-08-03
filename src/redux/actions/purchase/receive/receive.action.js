80afd090058b9e5b9387d78517e0f822fa4b96b4
anashrulyusuf <anashrulyusuf@gmail.com>
2020-08-03 18:05:09 +0700

    print pdf transaksi receive pembelian

--- src/redux/actions/purchase/receive/receive.action.js
+++ src/redux/actions/purchase/receive/receive.action.js
@@ -95,7 +95,7 @@ export const storeReceive= (data) => {
                     cancelButtonText: 'Oke!'
                 }).then((result) => {
                     if (result.value) {
-                        const win = window.open('http://google.com', '_blank');
+                        const win = window.open(data.result.nota,'_blank');
                         if (win != null) {
                             win.focus();
                         }
