f80705766904a44e13c6eceaa6595fb7cf3ce91e
anashrulyusuf <anashrulyusuf@gmail.com>
2020-08-06 14:36:12 +0700

    filter,pagination stock report

--- src/redux/actions/purchase/receive/receive.action.js
+++ src/redux/actions/purchase/receive/receive.action.js
@@ -204,64 +204,8 @@ export const FetchReceiveData = (nota,param='') => {
         axios.get(HEADERS.URL + `receive/ambil_data/${nota}`)
             .then(function (response) {
                 const data = response.data;
-                dispatch(setLoading(false));
-                console.log("ABUS ACTION");
-                if(param!==''){
-                    let master={
-                        "tgl_beli": data.result.master.tgl_beli,
-                        "no_faktur_beli": data.result.master.no_faktur_beli,
-                        "type": data.result.master.type,
-                        "nilai_pembelian": data.result.master.nilai_pembelian,
-                        "kode_supplier": data.result.master.kode_supplier,
-                        "tgl_jatuh_tempo": data.result.master.tgl_jatuh_tempo,
-                        "ppn": data.result.master.ppn,
-                        "dp": data.result.master.dp,
-                        "total_pembelian": data.result.master.total_pembelian,
-                        "pelunasan": data.result.master.pelunasan,
-                        "terbayar": data.result.master.terbayar,
-                        "operator": data.result.master.operator,
-                        "lokasi": data.result.master.lokasi,
-                        "bulat": data.result.master.bulat,
-                        "nonota": data.result.master.nonota,
-                        "no_po": data.result.master.no_po,
-                        "catatan": data.result.master.catatan,
-                        "disc": data.result.master.disc,
-                        "nama_penerima": data.result.master.nama_penerima,
-                        "no_pre_receive": data.result.master.no_pre_receive
-                    };
-                    let detail =  data.result.detail;
-                    // localStorage.setItem("data_master_receive",JSON.stringify(master));
-                    // localStorage.setItem("data_detail_receive",JSON.stringify(detail));
-                    // console.log(detail);
-
-                    data.result.detail.map((v,i)=>{
-                        const data_final={
-                            "kd_brg" : v.kode_barang,
-                            "barcode" : v.barcode,
-                            "satuan" : v.satuan,
-                            "diskon" : v.diskon,
-                            "harga_beli" : v.harga_beli,
-                            "stock" : v.stock,
-                            "diskon2" : 0,
-                            "diskon3" :0,
-                            "diskon4" : 0,
-                            "ppn" : v.ppn,
-                            "qty" : v.jumlah_beli,
-                            "qty_bonus" : v.jumlah_bonus,
-                            "nm_brg" : v.nm_brg,
-                            "tambahan" : v.tambahan,
-                        };
-                        store('receive', data_final);
-                    })
-
-                    // store('receive', data_final);
-
-                }
-                else{
                     dispatch(setPoData(data));
                     dispatch(setLoading(false));
-
-                }
             })
             .catch(function (error) {
                 // handle error
