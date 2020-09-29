36082ec36ac46e8d67fbdb65e7af946d63492b91
dayuss <ddtardian@gmail.com>
2020-09-29 15:38:22 +0700

    barcode file name

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 
 // document.getElementById("hellyeah").value = btoa('http://192.168.100.72:6692/');
-document.getElementById("hellyeah").value = btoa('http://localhost:3000/');
+document.getElementById("hellyeah").value = btoa('http://192.168.100.10:3000/');
 document.getElementById("coolyeah").value = btoa('Toko');
