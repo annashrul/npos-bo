81222c116eea344cfdbc3cdbe0fda29c6f19bafd
Your Name <you@example.com>
2021-07-01 14:54:16 +0700

    updateeeee bo penjualan

---
+++ dev/js/config.js
@@ -0,0 +1,5 @@
+
+//ieu url API
+document.getElementById("hellyeah").value = btoa('http://ptnetindo.com:6692/');
+//ieu value tenant anu dipake subdomain tea
+document.getElementById("coolyeah").value = ('npos');
