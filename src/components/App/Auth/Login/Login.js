08132af9991f2b65864df56b8f1d3b38f5f1d1d6
dayuss <ddtardian@gmail.com>
2021-09-01 17:25:45 +0700

    merge

--- src/components/App/Auth/Login/Login.js
+++ src/components/App/Auth/Login/Login.js
@@ -31,7 +31,7 @@ class Login extends Component {
     let parts = host.split(".");
     // const subdomains = btoa(parts[0]); 
     // const subdomains = btoa(document.getElementById("coolyeah").value);
-     const subdomains = btoa("miski");
+     const subdomains = btoa("npos");
     // const subdomains = btoa("cff");
     Cookies.set("tnt=", btoa(subdomains), {
       expires: 365,
