a94cd87d22185120adda3af18000a9aa993878d7
anashrul <anashrulyusuf@gmail.com>
2021-11-09 10:47:30 +0700

    fix pagination bayar piutang

--- src/components/App/Auth/Login/Login.js
+++ src/components/App/Auth/Login/Login.js
@@ -31,9 +31,9 @@ class Login extends Component {
     let parts = host.split(".");
     // const subdomains = btoa(parts[0]);
     // const subdomains = btoa(document.getElementById("coolyeah").value);
-    // const subdomains = btoa("miski");
-    const subdomains = btoa("npos");
-    // const subdomains = btoa("biant");
+    const subdomains = btoa("miski");
+    // const subdomains = btoa("npos");
+    // const subdomains = btoa("cff");
     Cookies.set("tnt=", btoa(subdomains), {
       expires: 365,
     });
